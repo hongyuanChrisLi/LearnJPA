8ba214233350c5f483c21277a66c7f3f24acee3e
Chris Li <kittokevin@gmail.com>
2016-12-30 14:24:41 -0800

    Entity Employee Test

--- src/core/service/EmployeeService.java
+++ src/core/service/EmployeeService.java
@@ -1,24 +1,51 @@
 package core.service;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import core.entity.Employee;
 
 public class EmployeeService {
-	
-	EntityManagerFactory emf =
-			Persistence.createEntityManagerFactory("EmployeeService");
-	
-	EntityManager em = emf.createEntityManager();
-	
+
+	protected EntityManager em;
+
+	public EmployeeService(EntityManager em) {
+		this.em = em;
+	}
+
 	public Employee createEmployee(int id, String name, long salary) {
 		Employee emp = new Employee(id);
 		emp.setName(name);
 		emp.setSalary(salary);
 		em.persist(emp);
 		return emp;
+	}
+
+	public Employee findEmployee(int id) {
+		return em.find(Employee.class, id);
+	}
+
+	public void removeEmployee(int id) {
+		Employee emp = em.find(Employee.class, id);
+		if (emp != null) {
+			em.remove(emp);
 		}
-	
+	}
+
+	public Employee raiseEmployeeSalary(int id, long raise) {
+		Employee emp = em.find(Employee.class, id);
+		if (emp != null) {
+			emp.setSalary(emp.getSalary() + raise);
+		}
+		return emp;
+	}
+
+	public List<Employee> findAllEmployees() {
+		TypedQuery<Employee> query = em.createQuery("SELECT e FROM Employee e", Employee.class);
+		return query.getResultList();
+	}
 }
